51d1cbc1bece3f95e54cad16caf6a1c079a9b9d6
Pedro az <ph7519490@gmail.com>
2025-10-18 13:30:03 -0300

    s

---
+++ Aulas/Aula15/arrays.js
@@ -0,0 +1,4 @@
+let num = [5, 8, 2, 4];
+num[4] = 1;
+num.push(9);
+console.log(`${num} e ${num.length} e ${num.sort()}`);
---
+++ Aulas/Aula15/arrays2.js
@@ -0,0 +1,9 @@
+let num = [8, 4, 4, 2, 1, 6, 9, 4, 2];
+
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+// }
+
+for (let i in num) {
+  console.log(num[i]);
+}
